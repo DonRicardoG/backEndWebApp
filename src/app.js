6d8d3b86043d111580fadd949d4b5f878f484024
Ricardo Gomez Aguirre <ricardog@Ricardos-MacBook-Air.local>
2023-12-21 19:56:57 -0600

    changes

--- src/app.js
+++ src/app.js
@@ -8,23 +8,20 @@ const app = express();
 
 app.disable("x-powered-by");
 
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(cookieParser());
-app.use(morgan("dev"));
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "postgresql://postgres:*1a2*12-C2CEB41dgcfe*EFfBG3BD-5d@viaduct.proxy.rlwy.net:28514/railway"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+// app.use(bodyParser.json({ limit: "50mb" }));
+// app.use(cookieParser());
+// app.use(morgan("dev"));
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 app.use(express.json());
 
--- src/database/db.js
+++ src/database/db.js
@@ -1,11 +1,5 @@
 import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize(
-  "railway",
-  "postgres",
-  "*1a2*12-C2CEB41dgcfe*EFfBG3BD-5d",
-  {
-    host: "viaduct.proxy.rlwy.net",
-    dialect: "postgres",
-  }
+  "postgresql://postgres:*1a2*12-C2CEB41dgcfe*EFfBG3BD-5d@viaduct.proxy.rlwy.net:28514/railway"
 );
