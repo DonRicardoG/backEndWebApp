35429dca47dcdecdb29f295fb6125bd9fb63cb46
Ricardo Gomez Aguirre <ricardog@Ricardos-MacBook-Air.local>
2023-12-21 19:03:24 -0600

    changes to the host

--- src/database/db.js
+++ src/database/db.js
@@ -1,5 +1,11 @@
 import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize(
-  "postgresql://postgres:*1a2*12-C2CEB41dgcfe*EFfBG3BD-5d@viaduct.proxy.rlwy.net:28514/railway;"
+  "railway",
+  "postgres",
+  "*1a2*12-C2CEB41dgcfe*EFfBG3BD-5d",
+  {
+    host: "viaduct.proxy.rlwy.net",
+    dialect: "postgres",
+  }
 );
