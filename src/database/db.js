704e0942b0732b33d902fb3711312d4b2bdfa5e6
Ricardo Gomez Aguirre <ricardog@Ricardos-MacBook-Air.local>
2024-01-09 11:18:43 -0600

    new DBURL 2

--- src/database/db.js
+++ src/database/db.js
@@ -5,5 +5,5 @@ const DB_URL = process.env.DB_URL;
 
 console.log(DB_URL);
 export const sequelize = new Sequelize(
-  "postgres://backend_webapp_user:bwt6CdJsKAG5zCWCA0a6peZjKqlMNk2M@dpg-cmemrnn109ks73c8obd0-a.oregon-postgres.render.com/backend_webapp"
+  "postgres://backend_webapp_user:bwt6CdJsKAG5zCWCA0a6peZjKqlMNk2M@dpg-cmemrnn109ks73c8obd0-a/backend_webapp"
 );
