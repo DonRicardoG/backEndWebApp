e5129bfcbcf34d41aed3299ae97080889004a466
Ricardo Gomez Aguirre <ricardog@Ricardos-MacBook-Air.local>
2024-01-09 12:05:35 -0600

    fixing CORS problem

--- src/app.js
+++ src/app.js
@@ -7,25 +7,25 @@ import "dotenv/config";
 
 const app = express();
 
-// app.disable("x-powered-by");
+app.disable("x-powered-by");
 
-// app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-// app.use(bodyParser.json({ limit: "50mb" }));
-// app.use(cookieParser());
-// app.use(morgan("dev"));
-// app.use((req, res, next) => {
-//   res.header(
-//     "Access-Control-Allow-Origin",
-//     "postgres://backend_webapp_user:bwt6CdJsKAG5zCWCA0a6peZjKqlMNk2M@dpg-cmemrnn109ks73c8obd0-a/backend_webapp"
-//   );
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//   next();
-// });
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "postgres://backend_webapp_user:bwt6CdJsKAG5zCWCA0a6peZjKqlMNk2M@dpg-cmemrnn109ks73c8obd0-a/backend_webapp"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 app.use(express.json());
 
