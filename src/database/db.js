6ef1273227ca309e99e06dd5c23c5e58f6258ff7
Ricardo Gomez Aguirre <ricardog@Ricardos-MacBook-Air.local>
2024-01-09 10:49:17 -0600

    changing db url

--- src/database/db.js
+++ src/database/db.js
@@ -3,4 +3,7 @@ import "dotenv/config";
 
 const DB_URL = process.env.DB_URL;
 
-export const sequelize = new Sequelize(`${DB_URL}`);
+console.log(DB_URL);
+export const sequelize = new Sequelize(
+  "postgres://backend_webapp_user:bwt6CdJsKAG5zCWCA0a6peZjKqlMNk2M@dpg-cmemrnn109ks73c8obd0-a/backend_webapp"
+);
